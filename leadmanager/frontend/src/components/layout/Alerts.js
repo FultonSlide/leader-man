7139a86ba2c54b77882137467e202e1768ba109c
Will <Wi_llo@hotmail.com>
2020-05-26 21:18:52 +1000

    test

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -20,7 +20,7 @@ export class Alerts extends Component {
 
     if (message !== prevProps.message) {
       if (message.deleteLead) alert.success(message.deleteLead);
-      if (message.addLead) alert.success(message.addLead);
+      if (message.addLead) alert.error(message.addLead);
     }
   }
 
